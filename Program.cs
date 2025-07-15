06e78bbbe9669bba554f88448a2d5d62b7917231
Edson <edin2008@yahoo.com.br>
2025-07-14 22:24:28 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Smartphone Nokia:");
+Nokia c1 = new Nokia("01234-5678", "5630", "354523458798635", 128);
+c1.Ligar();
+c1.ReceberLigacao();
+c1.InstalarAplicativo("WhatsApp");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Iphone i1 = new Iphone("98584-2525", "16 Pro Max", "354523458798635", 512);
+i1.Ligar();
+i1.ReceberLigacao();
+i1.InstalarAplicativo("Instagram");
